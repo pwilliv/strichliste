48d7a6f03b84b70e49f697f2dbc29f19b953e4bb
PWilhelm <pwilhelmv2@gmail.com>
2022-06-03 22:46:16 +0200

    Big refactor for upcoming changes
    Splited app into modules
    Added user login system

---
+++ strichliste/frontend/strichliste.py
@@ -0,0 +1,311 @@
+import json
+from datetime import datetime, timedelta
+
+import eventlet
+import argparse
+import random
+import hashlib
+from string import ascii_letters
+from flask import Flask, request, abort, current_app, render_template
+from whitenoise import WhiteNoise
+from models import db, User, Category, Product, Transaction
+from sys import exit
+from time import sleep
+
+# from flaskblog.config import Config
+
+#eventlet.monkey_patch()
+
+
+# # argument parsing
+# parser = argparse.ArgumentParser(description="Starts the selfhosting 'strichliste' server")
+# parser.add_argument(
+#     "-d", "--debug",
+#     action="store_true",
+#     help="enables Flasks debugger (don't ever set in production!)")
+# parser.add_argument(
+#     "-t", "--testing",
+#     action="store_true",
+#     help="set, if you want to execute strichliste in unit testing mode")
+# parser.add_argument(
+#     "--reset",
+#     action="store_true",
+#     help="if set, clears any records and initializes the database with default values.")
+# parser.add_argument(
+#     "-p", "--port",
+#     type=int,
+#     default=5000)
+# parser.add_argument(
+#     "--host",
+#     default="0.0.0.0")
+# parser.add_argument(
+#     "-db", "--dataBaseURI",
+#     dest="db",
+#     default="sqlite:////tmp/test.db"
+# )
+# parser.add_argument(
+#     "-psk",
+#     default="",
+#     help="The secret key to authenticate transactions with. defaults to \"\" (empty string)"
+# )
+# args = parser.parse_args()
+
+# DEBUG = args.debug                                  # default: False
+#TESTING = True  # args.testing                              # default: False
+# RESET = args.reset                                  # default: False
+# PORT = args.port                                    # default: 5000
+# HOST = args.host                                    # default: "0.0.0.0"
+# SQLALCHEMY_DATABASE_URI = args.db                   # default: "sqlite:////tmp/test.db"
+# PSK = args.psk                                      # default: ""
+
+# always use a in-memory db, when testing to avoid data loss due to human error
+# if TESTING:
+#     SQLALCHEMY_DATABASE_URI = "sqlite://"
+
+
+# def create_app(config_class=Config):
+#     app = Flask(__name__)
+#     app.config.from_object(Config)
+#     from strichliste.backend.routes import backend
+#
+#     app.register_blueprint(backend)
+#
+#     app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+#     print("connected to: " + SQLALCHEMY_DATABASE_URI)
+#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#
+#     db.init_app(app)
+#     login_manager.init_app(app)
+#     app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
+#     return app
+
+
+#create_app().app_context().push()
+
+
+# @current_app.route("/reset")
+# def reset():
+#     if TESTING:
+#         init_with_dummy_data(True)
+#         return "ok"
+#     else:
+#         abort(404)
+#
+#
+# def init_with_dummy_data(overwrite_safety=False):
+#     assert TESTING \
+#            or overwrite_safety \
+#            or ("YES" == input("Are your sure to delete all records and reset the database? [YES/NO]"))
+#     db.drop_all()
+#     db.create_all()
+#     coleur = User("Coleur")
+#     cat_a = Category("A", 0.4)
+#     cat_b = Category("B", 0.45)
+#     cat_c = Category("C", 0.8)
+#     cat_d = Category("D", 1.1)
+#     ötti = Product(
+#         name="Öttinger",
+#         category=cat_b,
+#         bulk_size=20)
+#     # transaction_1 = Transaction(user=erik, category=cat_b, amount=5, timestamp=datetime.now(), undone=False)
+#     [db.session.add(entity) for entity in (coleur, cat_a, cat_b, cat_c, cat_d, ötti)]
+#     db.session.commit()
+#
+#
+# def jsonfy_users(users):
+#     def extract_user_info(user: User):
+#         # we only want some fields as there are some SQLAlchemy-generated fields, we don't want to return.
+#         response = dict()
+#         response["id"] = user.id
+#         response["name"] = user.name
+#         response["locked"] = user.locked
+#         return response
+#
+#     return json.dumps(list(map(extract_user_info, users)), sort_keys=True, indent=4)
+#
+#
+# def get_user_balance(user_id: int,
+#                      from_date: datetime = datetime.min,
+#                      until_date: datetime = datetime.max) -> float:
+#     user = User.query.filter(User.id == user_id).first_or_404()
+#     return round(
+#         sum(
+#             map(lambda x: x.price(),
+#                 get_transactions_of_user(user=user,
+#                                          from_date=from_date,
+#                                          until_date=until_date))
+#         )
+#         , 2)
+#
+#
+# def get_transactions_of_user(user, from_date, until_date):
+#     return list(  # for some reason this is needed
+#         filter(lambda transaction: transaction.user_id == user.id,
+#                Transaction.query \
+#                .filter(Transaction.undone == False,
+#                        Transaction.timestamp > from_date,
+#                        Transaction.timestamp < until_date)
+#                .all()
+#                )
+#     )
+#
+#
+# def get_number_of_purchases(user: User,
+#                             category: Category,
+#                             from_date: datetime = datetime.min,
+#                             until_date: datetime = datetime.max) -> int:
+#     transactions = get_transactions_of_user(user=user,
+#                                             from_date=from_date,
+#                                             until_date=until_date)
+#     return sum(
+#         map(lambda x: x.amount,
+#             filter(lambda transaction: transaction.category == category, transactions)
+#             )
+#     )
+#
+#
+#
+#
+# @current_app.route("/balances")
+# def balances():
+#     # parse date constrains
+#     try:
+#         begin_date = datetime.strptime(request.args["begin"], "%Y-%m-%d")
+#     except:
+#         begin_date = datetime.min
+#
+#     try:
+#         end_date = datetime.strptime(request.args["end"], "%Y-%m-%d")
+#         end_date = end_date.replace(hour=23, minute=59, second=59, microsecond=999999)
+#     except:
+#         end_date = datetime.max
+#
+#     # fetch data and render template
+#     humans = list()
+#     [humans.append((user.name, str(get_user_balance(user.id, begin_date, end_date)) + "€"))
+#      for user in User.query.order_by(User.name).all()]
+#
+#     return render_template("backend.html",
+#                            humans=humans,
+#                            scaling="100%")  # ugly, semi-functional and currently only used for fiddling
+#
+#
+# challenge = None
+#
+#
+# @current_app.route("/challenge")
+# def get_crypto_challenge():
+#     "returns the current challenge used to verify transactions. regenerate after *every* use."
+#     global challenge
+#     if challenge is None:
+#         challenge = "".join(random.SystemRandom().choice(ascii_letters) for _ in range(42))
+#     return challenge
+#
+#
+# def check_transaction(transaction: str, hash: str, psk: str = PSK) -> bool:
+#     """
+#     validates a transaction. afterwards discards the current challenge and generates a new one.
+#     a valid transaction hash is
+#         sha512(transaction+challenge+psk) as hexdigests.
+#     , with challenge being supplied by get_crypto_challenge() and the transaction string representing *all* properties
+#     of the transaction in a *unambiguous* way.
+#     """
+#     global challenge
+#     correct_response = hashlib.sha512(str(transaction + get_crypto_challenge() + psk).encode("utf-8")).hexdigest()
+#     print(correct_response)
+#     challenge = "".join(random.SystemRandom().choice(ascii_letters) for _ in range(42))
+#     return hash == correct_response
+#
+#
+# @current_app.route("/add_transaction/<user_id>/<category_id>/<amount>/<checksum>")
+# def add_transaction(user_id: str, category_id: str, amount: int, checksum: str):
+#     valid = check_transaction(
+#         transaction="/add_transaction/" + user_id + "/" + category_id + "/" + str(amount),
+#         hash=checksum)
+#
+#     if valid is False:
+#         abort(403)
+#
+#     amount = int(amount)
+#     if amount < 1:
+#         return "oh you."
+#     user = User.query.filter(User.id == user_id).first_or_404()
+#     category = Category.query.filter(Category.id == category_id).first_or_404()
+#     transaction = Transaction(user=user,
+#                               category=category,
+#                               timestamp=datetime.now(),
+#                               amount=amount,
+#                               undone=False)
+#     db.session.add(transaction)
+#     db.session.commit()
+#     return "ok"
+#
+#
+# @current_app.route("/get_user_balance/<user_id>")
+# def user_balance_wrapper(user_id):
+#     return str(get_user_balance(user_id))
+#
+#
+# @current_app.route("/get_number_of_purchases/<int:user_id>/<int:category_id>")
+# def number_of_purchases_wrapper(user_id: int, category_id: int):
+#     user = User.query.filter(User.id == user_id).first_or_404()
+#     category = Category.query.filter(Category.id == category_id).first_or_404()
+#     return str(get_number_of_purchases(user, category))
+#
+#
+# @current_app.route("/get_all_users")
+# def get_all_users():
+#     users = User.query.order_by(User.name).all()
+#     return jsonfy_users(users)
+#
+#
+# @current_app.route("/get_user_by_name/<name>")
+# def get_user_by_name(name: str):
+#     user = User.query.filter(User.name == name).first_or_404()
+#     return jsonfy_users([user])
+#
+#
+# @current_app.route("/add_user/<new_name>/<checksum>")
+# def add_user(new_name: str, checksum: str):
+#     if check_transaction("add_user/" + new_name, checksum) is False:
+#         abort(403)
+#
+#     user = User(new_name)
+#     if User.query.filter(User.name == new_name).first() is not None:
+#         return "{'Error': 'User with given name already exists'}"
+#     db.session.add(user)
+#     db.session.commit()
+#     return "Success!"
+#
+#
+# @current_app.route("/undo/<checksum>")
+# def undo(checksum: str):
+#     """Undoes the latest (by time) transaction"""
+#     if check_transaction("undo", checksum) is False:
+#         abort(403)
+#     transaction = Transaction.query \
+#         .filter(Transaction.undone == False) \
+#         .order_by(Transaction.timestamp.desc()).first_or_404()
+#     # don't undo changes that happened more than 20s in the past
+#     if transaction.timestamp + timedelta(0, 20) < datetime.now():
+#         abort(404)
+#     transaction.undone = True
+#     db.session.add(transaction)
+#     db.session.commit()
+#     return "ok"
+#
+# @current_app.route("/stop")
+# def stop():
+#     # its surprisingly hard to force quit subprocesses in python, so this acts as a peaceful way to stop testing servers
+#     if TESTING:
+#         try:
+#             return "OK"
+#         finally:
+#             sleep(0.5)
+#             exit(0)
+#     abort(404)
+
+# if __name__ == "__main__":
+#     if RESET or TESTING:
+#         init_with_dummy_data()
+#     current_app.run(debug=DEBUG, host=HOST, port=PORT)
--- strichliste/models.py
+++ strichliste/models.py
@@ -1,10 +1,65 @@
+from flask import current_app
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
+from strichliste.extensions import db, login_manager
+from flask_login import UserMixin
+#from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+import jwt
 
-db = SQLAlchemy()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    def get_reset_token(self, expiration=1800):
+        reset_token = jwt.encode(
+            {
+                "confirm": self.id,
+                "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+                       + datetime.timedelta(seconds=expiration)
+            },
+            current_app.config['SECRET_KEY'],
+            algorithm="HS256"
+        )
+        return reset_token
+
+    @staticmethod
+    def verify_reset_token(self, token):
+        try:
+            data = jwt.decode(
+                token,
+                current_app.config['SECRET_KEY'],
+                leeway=datetime.timedelta(seconds=10),
+                algorithms=["HS256"]
+            )
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Consumer(db.Model):
     # models a user by their name.
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text, unique=True, nullable=False)
@@ -15,7 +70,7 @@ class User(db.Model):
         self.locked = False
 
     def __repr__(self):
-        return '<User %r>' % self.name
+        return '<Consumer %r>' % self.name
 
 
 class Category(db.Model):
@@ -50,16 +105,16 @@ class Product(db.Model):
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    consumer_id = db.Column(db.Integer, db.ForeignKey("consumer.id"), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey("category.id"), nullable=False)
-    user = db.relationship("User", backref="transactions")
+    user = db.relationship("Consumer", backref="transactions")
     category = db.relationship("Category", backref="transactions")
     amount = db.Column(db.Integer, nullable=False)
     undone = db.Column(db.Boolean, nullable=False)
 
-    def __init_(self, user: User, category: Category, timestamp: datetime, amount: int = 1) -> None:
+    def __init_(self, consumer: Consumer, category: Category, timestamp: datetime, amount: int = 1) -> None:
         self.undone = False
-        self.user = user
+        self.consumer = consumer
         self.category = category
         self.timestamp = timestamp
         self.amount = amount
@@ -68,4 +123,4 @@ class Transaction(db.Model):
         return self.category.price * self.amount
 
     def __repr__(self):
-        return "<Transaction %r - %r>" % (self.user.name, self.product.name)
+        return "<Transaction %r - %r>" % (self.consumer.name, self.product.name)
